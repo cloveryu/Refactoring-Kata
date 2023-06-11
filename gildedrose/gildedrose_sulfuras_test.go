9df0af47738cfb9633ed58dc985c0757fe2e4a12
yangyu <yangyu@thoughtworks.com>
2023-06-11 17:11:31 +0800

    add test for sulfuras

---
+++ gildedrose/gildedrose_sulfuras_test.go
@@ -0,0 +1,18 @@
+package gildedrose_test
+
+import (
+	"clover.yu/refactoring-kata/gildedrose"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_sulfuras_quality_and_sellIn_not_change_when_update(t *testing.T) {
+	var items = []*gildedrose.Item{
+		{"Sulfuras, Hand of Ragnaros", 3, 66},
+	}
+
+	gildedrose.UpdateQuality(items)
+
+	assert.Equal(t, 3, items[0].SellIn)
+	assert.Equal(t, 66, items[0].Quality)
+}
