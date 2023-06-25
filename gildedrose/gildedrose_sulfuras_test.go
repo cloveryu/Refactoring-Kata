40d3066a86c51ca3a9ae2d2a1845726e17f1ee85
yangyu <yangyu@thoughtworks.com>
2023-06-25 10:06:21 +0800

    fix sulfuras quality 80

--- gildedrose/gildedrose_sulfuras_test.go
+++ gildedrose/gildedrose_sulfuras_test.go
@@ -8,11 +8,11 @@ import (
 
 func Test_sulfuras_quality_and_sellIn_not_change_when_update(t *testing.T) {
 	var items = []*gildedrose.Item{
-		{"Sulfuras, Hand of Ragnaros", 3, 66},
+		{"Sulfuras, Hand of Ragnaros", 3, 80},
 	}
 
 	gildedrose.UpdateQuality(items)
 
 	assert.Equal(t, 3, items[0].SellIn)
-	assert.Equal(t, 66, items[0].Quality)
+	assert.Equal(t, 80, items[0].Quality)
 }
